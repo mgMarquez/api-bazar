acf0be74eae9306ec1ea776abb2bfb5553b6e163
Mauricio Marquez <mgmarquezsilva@gmail.com>
2022-09-06 16:58:28 -0300

    primer commit

---
+++ src/main/java/com/yoprogramo/apibazar/ApibazarApplication.java
@@ -0,0 +1,13 @@
+package com.yoprogramo.apibazar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApibazarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApibazarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yoprogramo/apibazar/ApibazarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoprogramo.apibazar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApibazarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
