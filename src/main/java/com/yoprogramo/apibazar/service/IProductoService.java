75bf1bac9415dadee88b2220ed911dee76f8eead
Mauricio Marquez <mgmarquezsilva@gmail.com>
2022-09-06 17:53:31 -0300

    Nueva interfaz producto service

---
+++ src/main/java/com/yoprogramo/apibazar/service/IProductoService.java
@@ -0,0 +1,13 @@
+package com.yoprogramo.apibazar.service;
+
+import com.yoprogramo.apibazar.model.Producto;
+
+import java.util.List;
+
+public interface IProductoService {
+    void guardarProducto(Producto producto);
+    List<Producto> obtenerProductos();
+    void eliminarProducto(Long id);
+    void modificarProducto(Producto producto);
+    Producto obtenerProductoPorId(Long id);
+}
